811ccd8c9c7ad17284812e63c5c28e4d6b867dce
Brian Gomes <briangomes@sapo.pt>
2021-12-04 12:49:33 +0000

    Adicionar arquivos de projeto.

---
+++ NumerosPrimos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosPrimos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Insira um número:");
+            int numero = int.Parse(Console.ReadLine());
+            
+            int numerodivisoes = 0;
+
+            for (int contador = 1; contador <= numero; contador++) {
+
+                //int resultado = numero % contador;
+
+                if (numero % contador == 0) {
+                    numerodivisoes++;
+                }
+
+            }
+
+            if (numerodivisoes == 2){
+
+                Console.WriteLine($"O múmero {numero} é primo!");
+
+            } else {
+
+                Console.WriteLine($"O múmero {numero} não primo!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+    
+
